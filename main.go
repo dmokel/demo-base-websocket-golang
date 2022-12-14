1f29034ad6eb97eca5d7a4bc6175c6ae0534c2b3
David Mokel <Moqqll.china@gmail.com>
2022-12-15 00:09:32 +0800

    feat(examples): add cmd examples using websocket

--- main.go
+++ main.go
@@ -1,40 +1,10 @@
 package main
 
-import chatexample "github.com/dmokel/demo-base-websocket-golang/chat-example"
-
-// var upgrader = websocket.Upgrader{
-// 	ReadBufferSize:  1024,
-// 	WriteBufferSize: 1024,
-// }
-
-// type handlerForSome struct{}
-
-// func (h handlerForSome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	conn, err := upgrader.Upgrade(w, r, nil)
-// 	if err != nil {
-// 		fmt.Println("err:", err)
-// 		return
-// 	}
-
-// 	for {
-
-// 	}
-// }
+import (
+	cmdexample "github.com/dmokel/demo-base-websocket-golang/cmd-example"
+)
 
 func main() {
-	chatexample.CoreLogic()
-	// http.Handle("/handle", &handlerForSome{})
-	// http.HandleFunc("/handle-func", func(w http.ResponseWriter, r *http.Request) {
-	// 	conn, err := upgrader.Upgrade(w, r, nil)
-	// 	if err != nil {
-	// 		fmt.Println("err:", err)
-	// 		return
-	// 	}
-
-	// 	for {
-
-	// 	}
-	// })
-
-	// http.ListenAndServe("0.0.0.0", nil)
+	// chatexample.CoreLogic()
+	cmdexample.Logic()
 }
